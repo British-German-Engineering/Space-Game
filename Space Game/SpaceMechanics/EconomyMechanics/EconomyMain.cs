2568c79a2b401401d515288254d54182f9ba75bd
TheKettleEngine <95769934+TheKettleEngine@users.noreply.github.com>
2023-05-30 17:22:26 +0100

    just made the base files needed for the economy and politics system

---
+++ Space Game/SpaceMechanics/EconomyMechanics/EconomyMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// this code is for handeling all the other economy modules and allowing
+// for ease of use amongst all other parts of the game that require the economy
+
+namespace Space_Game.SpaceMechanics.EconomyMechanics
+{
+    internal class EconomyMain
+    {
+    }
+}
