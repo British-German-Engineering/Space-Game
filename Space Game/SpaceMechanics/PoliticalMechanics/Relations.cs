c2decfa5b4131608a7ebc9f3a1f219d84cd1b693
TheKettleEngine <95769934+KettleEngine@users.noreply.github.com>
2023-06-04 12:33:36 +0100

    made a relationships file
    it is going to be the job of this file to handle anything relating to relationships between factions.

---
+++ Space Game/SpaceMechanics/PoliticalMechanics/Relations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Relationships
+{
+	//here to initalise relations between whatever it is, so this is here to establish the inital relation value when a new faction is created
+	public RelationsInit()
+	{
+	}
+}
